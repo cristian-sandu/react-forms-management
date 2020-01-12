43c5060bd5e9571364227249388a4302fac5da25
csandu <sandu.cristian@mail.ru>
2020-01-12 15:18:00 +0200

    Admin Dashboard

---
+++ app/containers/AdminDashboard/constants/index.js
@@ -0,0 +1,20 @@
+export const TABS = {
+  REGISTRATION: {
+    NAME: 'Registration',
+  },
+  ENTITY_BY_NAME: {
+    NAME: 'Entity By Name',
+  },
+  ENTITY_BY_ID: {
+    NAME: 'Entity By ID',
+  },
+  ASSOCIATION_BY_NAME: {
+    NAME: 'Association By Name',
+  },
+  ENTITIES: {
+    NAME: 'Entitties',
+  },
+  ASSOCIATIONS: {
+    NAME: 'Associations',
+  },
+};
--- app/containers/AdminDashboard/index.js
+++ app/containers/AdminDashboard/index.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import { Tabs } from 'antd';
+import { StickyContainer, Sticky } from 'react-sticky';
 
-const AdminDashboard = () => {
-  return <span> ADMIN </span>;
-};
+import { TABS } from './constants';
+
+const { TabPane } = Tabs;
+
+const renderTabBar = (props, DefaultTabBar) => (
+  <Sticky bottomOffset={80}>
+    {({ style }) => (
+      <DefaultTabBar
+        {...props}
+        style={{ ...style, zIndex: 1, background: '#fff' }}
+      />
+    )}
+  </Sticky>
+);
+
+const AdminDashboard = () => (
+  <StickyContainer>
+    <Tabs defaultActiveKey="1" renderTabBar={renderTabBar}>
+      <TabPane
+        tab={TABS.REGISTRATION.NAME}
+        key={TABS.REGISTRATION.NAME}
+        style={{ height: 200 }}
+      >
+        REGISTRATION
+      </TabPane>
+      <TabPane tab={TABS.ENTITIES.NAME} key={TABS.ENTITIES.NAME}>
+        ENTITIES
+      </TabPane>
+      <TabPane tab={TABS.ENTITY_BY_ID.NAME} key={TABS.ENTITY_BY_ID.NAME}>
+        ENTITY_BY_ID
+      </TabPane>
+      <TabPane tab={TABS.ASSOCIATIONS.NAME} key={TABS.ASSOCIATIONS.NAME}>
+        ASSOCIATIONS
+      </TabPane>
+      <TabPane
+        tab={TABS.ASSOCIATION_BY_NAME.NAME}
+        key={TABS.ASSOCIATION_BY_NAME.NAME}
+      >
+        ASSOCIATION_BY_NAME
+      </TabPane>
+    </Tabs>
+  </StickyContainer>
+);
 
 export default AdminDashboard;
